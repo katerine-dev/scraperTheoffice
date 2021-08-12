18d8f98b30783053bb7f62c49327056aca40ea19
kat_wit <ka.lwitkoski@gmail.com>
2021-08-11 22:29:16 -0300

    finaliza teste principal

--- R/theoffice_test.R
+++ R/theoffice_test.R
@@ -7,6 +7,54 @@ url_completa <- paste0(URL_BASE, endpoint)
 response <-
   httr::GET(url_completa, httr::write_disk("output/theoffice.html"))
 
-html_file <- readr::read_file("output/theoffice.html")
+html_file <- rvest::read_html("output/theoffice.html")
 
+library(tidyverse)
+library(tidyselect)
 
+tabelas <-  html_file |>
+  rvest::html_nodes("table.wikitable.noprint.plainlinks.unsortable.uncollapsible") |>
+  rvest::html_table() |>
+  tibble::enframe() |>
+  dplyr::mutate(value = purrr::map(
+    value,
+    setNames,
+    c(
+      "n_total",
+      "n_episodio",
+      "titulo",
+      "dirigido_por",
+      "escrito_por",
+      "audiencia",
+      "codigo",
+      "exibicao_original"
+    )
+  )) |>
+  dplyr::mutate(value = purrr::map(value, ~ dplyr::mutate(.x, n_total = as.character(n_total)))) |>
+  dplyr::mutate(value = purrr::map(value, ~ dplyr::mutate(.x, n_episodio = as.character(n_episodio)))) |>
+  dplyr::mutate(value = purrr::map(value, ~ dplyr::mutate(.x, codigo = as.character(codigo)))) |>
+  tidyr::unnest(value)
+
+tabelas_restante <- html_file |>
+  rvest::html_nodes("table.wikitable.plainrowheaders") |>
+  rvest::html_table() |>
+  tibble::enframe() |>
+  dplyr::mutate(value = purrr::map(
+    value,
+    setNames,
+    c(
+      "n_total",
+      "n_episodio",
+      "titulo",
+      "dirigido_por",
+      "escrito_por",
+      "audiencia",
+      "codigo",
+      "exibicao_original"
+    )
+  )) |>
+  tidyr::unnest(value) |>
+  dplyr::filter(!str_detect(n_total, "[a-zA-Z]"))
+
+tabelas_prontas <- tabelas |>
+  dplyr::full_join(tabelas_restante)
