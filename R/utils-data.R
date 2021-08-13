4d57df98724ddc674fb1f0cb097f6c26d7c66ca8
kat_wit <ka.lwitkoski@gmail.com>
2021-08-13 13:45:52 -0300

    resolve probleminha no pacote

---
+++ R/utils-data.R
@@ -0,0 +1,19 @@
+#' baseTheoffice
+#'
+#' Um conjunto de dados que contem informacoes sobre os episódios da serie The Office´.
+#'
+#' @format um data frame com 163 linhas e 9 variaveis:
+#'
+#' \describe{
+#'   \item{temporada}{temporada do episodio.}
+#'   \item{n_total}{numero do episodio correspondente ao total de todas as temporadas.}
+#'   \item{n_episodio}{numero do episodio correspondente a temporada.}
+#'   \item{dirigido_por}{diretor do episodio.}
+#'   \item{escrito_por}{escritor do roteiro do episodio.}
+#'   \item{audiencia}{numero da audiencia na data de exibicao original.}
+#'   \item{codigo}{codigo referente ao episodio.}
+#'   \item{exibicao_original}{a data da exibicao original do episodio.}
+#' }
+#'
+#' @source baseTheoffice
+"baseTheoffice"
